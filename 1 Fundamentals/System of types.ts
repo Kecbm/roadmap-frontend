b9885188add200ab8480a11c38fab1f7d2300dc2
Klecianny Melo <klecianny.melo@quorum.us>
2026-03-25 19:46:50 -0300

    :memo: generics

---
+++ 1 Fundamentals/System of types.ts
@@ -0,0 +1,14 @@
+// SISTEMA DE TIPOS
+
+// GENERICS
+/*
+  Pode ser compreendido como "variáveis para tipos". Permite a criação de componentes que podem funcionar com vários tipos, em vez de um único tipo.
+*/
+
+// Funções genéricas
+
+// CONDITIONAL TYPES
+
+// MAPPED TYPES
+
+// TEMPLATE LITERAL TYPES
