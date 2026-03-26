a4d52232e6282387a6509a9d1ab4ff5c7d857ca8
Klecianny Melo <klecianny.melo@quorum.us>
2026-03-26 19:43:35 -0300

    :memo: utility types

---
+++ 1 Fundamentals/1 TypeScript/2. Utility types.ts
@@ -0,0 +1,14 @@
+// 🚀 Utility types
+/*
+  Ferramentas que transformam tipos existentea de forma rápida e elegante. Semelhante a uma "função de tipo", que recebem um tipo e devolvem uma versão modificada dele
+*/
+
+// 👩🏾‍🚀 PICK
+
+// 👩🏾‍🚀 OMIT
+
+// 👩🏾‍🚀 RECORD
+
+// 👩🏾‍🚀 PARTIAL
+
+// 👩🏾‍🚀 REQUIRED
