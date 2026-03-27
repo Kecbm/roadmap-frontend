623df9f0be64fc3dc6ae8ff4f94d8432e2bb1849
Klecianny Melo <klecianny.melo@quorum.us>
2026-03-27 18:39:13 -0300

    :memo: type guards

---
+++ 1 Fundamentals/1 TypeScript/3. Type safety through control flow analysis.ts
@@ -0,0 +1,10 @@
+// 🚀 Type Safety through Control Flow Analysis
+
+// 👩🏾‍🚀 Type guards
+/*
+  São expressões que verificam o tipo de uma variável em tempo de execução
+*/
+
+// 👩🏾‍🚀 Discriminated unions
+
+// 👩🏾‍🚀 Type narrowing
