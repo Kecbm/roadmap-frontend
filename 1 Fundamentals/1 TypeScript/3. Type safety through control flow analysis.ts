cc2a2ecb594630f5fa776e62f55f66430a1e2551
Klecianny Melo <klecianny.melo@quorum.us>
2026-03-27 18:45:15 -0300

    :sparkles: type guards

--- 1 Fundamentals/1 TypeScript/3. Type safety through control flow analysis.ts
+++ 1 Fundamentals/1 TypeScript/3. Type safety through control flow analysis.ts
@@ -4,6 +4,20 @@
 /*
   São expressões que verificam o tipo de uma variável em tempo de execução
 */
+// ✨ Nativos: `typeof`, `instanceof` e `in`
+
+// ✨ Customizados
+// Encapsulamento de uma lógica em uma função
+interface Passaro { voar: () => void }
+interface Peixe { nadar: () => void }
+
+function ehPassaro(animal: Passaro | Peixe): animal is Passaro {
+    return (animal as Passaro).voar !== undefined;
+}
+
+if (ehPassaro(pet)) {
+    pet.voar();
+}
 
 // 👩🏾‍🚀 Discriminated unions
 
