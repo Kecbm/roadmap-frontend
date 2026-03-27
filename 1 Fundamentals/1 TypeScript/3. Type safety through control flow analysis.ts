70ad96f63863e1e8c9e518b8c1c2f90f53b0f7e4
Klecianny Melo <klecianny.melo@quorum.us>
2026-03-27 19:37:55 -0300

    :sparkles: example of discriminated unions

--- 1 Fundamentals/1 TypeScript/3. Type safety through control flow analysis.ts
+++ 1 Fundamentals/1 TypeScript/3. Type safety through control flow analysis.ts
@@ -1,12 +1,12 @@
-// 🚀 Type Safety through Control Flow Analysis
+// 🚀 TYPE SAFETY THROUGH CONTROL FLOW ANALYSIS
 
 // 👩🏾‍🚀 Type guards
 /*
   São expressões que verificam o tipo de uma variável em tempo de execução
 */
-// ✨ Nativos: `typeof`, `instanceof` e `in`
+// 🛸 Nativos: `typeof`, `instanceof` e `in`
 
-// ✨ Customizados
+// 🛸 Customizados
 // Encapsulamento de uma lógica em uma função
 interface Passaro { voar: () => void }
 interface Peixe { nadar: () => void }
@@ -19,7 +19,7 @@ if (ehPassaro(pet)) {
     pet.voar();
 }
 
-// 👩🏾‍🚀 Discriminated unions
+// 👩🏾‍🚀 DISCRIMINATEDED UNIONS
 /*
   Criar tipos que compartilham uma identidade comum
 */
@@ -46,7 +46,7 @@ function tratarResposta(res: Resposta) {
     }
 }
 
-// 👩🏾‍🚀 Type narrowing
+// 👩🏾‍🚀 TYPE NARROWING
 /*
   É o ato de "afunilar" um tipo. Antes de aplicar métodos nativos em uma variável, o TS faz uma validação de que ela realmente tem aquele tipo no momento
 */
@@ -57,4 +57,49 @@ function repetir(valor: string | number) {
     }
     // Se chegou aqui, o TS sabe que 'valor' é number
     return valor * 3;
+}
+
+// 🪐 EXEMPLO PRÁTICO
+/*
+  Gerenciamento do estado de uma requisição para buscar usuários de uma API
+*/
+
+// ☄️ O problema
+interface EstadoRuim {
+    loading: boolean;
+    data?: string[];
+    error?: string;
+}
+// O TS permite { loading: true, data: ["Usuário 1"] }, o que não faz sentido
+
+// 🔭 A solução: discriminated unions
+/*
+  O `status` irá "discriminar" (separar) os tipos
+*/
+// 1. Definimos os tipos específicos para cada momento
+interface Idle { status: "parado" }
+interface Loading { status: "carregando" }
+interface Sucess { status: "sucesso"; dados: string[] }
+interface Error { status: "erro"; mensagem: string }
+
+// 2. Criamos a união descriminada
+type EstadoRequisicao = Idle | Loading | Sucess | Error;
+
+function renderizarPagina(estado: EstadoRequisicao) {
+    // 3. O Type Narrowing acontece dentro do switch
+    switch (estado.status) {
+        case "parado":
+            return "Clique para buscar usuários";
+
+        case "carregando":
+            return "Carregando...";
+
+        case "sucesso":
+            // O TS sabe que existe 'dados'
+            return `Lista: ${estado.dados.join(", ")}`;
+
+        case "erro":
+            // O TS sabe que existe 'mensagem'
+            return `Ops! Algo deu errado: ${estado.mensagem}`
+    }
 }
