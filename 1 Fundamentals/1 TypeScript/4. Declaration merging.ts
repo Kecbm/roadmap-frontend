577acb475922bb1b299b37e01c4185adbd6aa0da
Klecianny Melo <klecianny.melo@quorum.us>
2026-03-28 14:48:55 -0300

    :sparkles: declaration merging

---
+++ 1 Fundamentals/1 TypeScript/4. Declaration merging.ts
@@ -0,0 +1,23 @@
+// 🚀 DECLARATION MERGING
+/*
+  Quando o compilador do TS encontra duas ou mais declarações com o mesmo nome no mesmo namespace. No lugar de gerar o erro de "nome dulicado", ele combina as definições em uma única
+*/
+// Se declarar a mesma interface duas vezes, o TS une os membros de ambas
+interface Usuario {
+    nome: string;
+}
+
+interface Usuario {
+    idade: number;
+}
+
+// O resultado final é: { nome: string; idade: number }
+const pessoa: Usuario = {
+    nome: "Lucas",
+    idade: 25
+}
+
+// Regra: se ambas as interfaces tiverem uma propriedade com o mesmo nome, o tipo deve ser idêntico, ou o compilador emitirá um erro
+// Funções e namespaces: também podem ser mesclados, é possivel adicionar propriedades a uma função declarando um namespace com o mesmo nome dela
+
+// 👩🏾‍🚀 MODULE AGMENTATION
