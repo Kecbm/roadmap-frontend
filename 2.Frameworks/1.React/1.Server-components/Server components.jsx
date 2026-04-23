4d1fe24a23f209e6f6834c2699bd1c7c3eedd0f6
Klecianny Melo <klecianny.melo@quorum.us>
2026-04-23 20:57:41 -0300

    :sparkles: server components

---
+++ 2.Frameworks/1.React/1.Server-components/Server components.jsx
@@ -0,0 +1,10 @@
+/* 🚀 REACT SERVER COMPONENTS (RSC) */
+
+/* 👩🏾‍🚀 O QUE SÃO SERVER COMPONENTS? */
+/*
+  O React era uma biblioteca client side. Com o React 19, ela se tornou multi-ambiente.
+
+  Os Server Components, são componentes que executam exclusivamente no servidor. Não geram um bundle de JS no navegador.
+
+  O servidor executa o componente, resolve a lógica e envia para o cliente um formato de dados chamado RSC Payload.
+*/
