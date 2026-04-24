160a88bc36d27d4f2beac573df66c8ee8c50e1b1
Klecianny Melo <klecianny.melo@quorum.us>
2026-04-23 21:02:10 -0300

    :sparkles: comunication structure

--- 2.Frameworks/1.React/1.Server-components/Server components.jsx
+++ 2.Frameworks/1.React/1.Server-components/Server components.jsx
@@ -8,3 +8,11 @@
 
   O servidor executa o componente, resolve a lógica e envia para o cliente um formato de dados chamado RSC Payload.
 */
+
+/* 👩🏾‍🚀 ESTRUTURA DE COMUNICAÇÃO */
+/*
+  O React 19 não envia HTML puro do servidor para o cliente. Ele envia um fluxo (stream) que descreve a árvore de componentes.
+
+  1. Servidor: Executa o componente e gera o JSON-like payload
+  2. Cliente: Recebe esse payload e reconstrói a árvore de componentes, preservando o estado de qualquer componente de cliente que já estivesse na tela.
+*/
