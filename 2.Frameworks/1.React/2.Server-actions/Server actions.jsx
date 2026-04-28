2c3947a799c43ed2d9c38ffd6f52182f66a84113
Klecianny Melo <klecianny.melo@quorum.us>
2026-04-27 21:18:12 -0300

    :sparkles: RPC

--- 2.Frameworks/1.React/2.Server-actions/Server actions.jsx
+++ 2.Frameworks/1.React/2.Server-actions/Server actions.jsx
@@ -10,3 +10,12 @@
 
   Com Server Actions, você define uma função assíncrona com a diretiva "use server". O React entende que, ao chamar essa função, ele deve fazer uma requisição HTTP automática para o servidor, executar a lógica lá e devolver o resultado
 */
+
+/* 👩🏾‍🚀 COMO FUNCIONA A PONTE "RPC" */
+/*
+  Quando você define uma Server Action, o React cria um RPC (Remote Procedure Call).
+
+  1. No Cliente: O navegador envia um pacote de dados (geralmente FormData).
+  2. No Servidor: O React recebe esse pacote, identifica qual função deve ser executada e a roda no ambiente Node.js.
+  3. Resposta: O servidor pode retornar dados, mensagens de erro ou até instruir o React a atualizar partes da página (revalidation).
+*/
