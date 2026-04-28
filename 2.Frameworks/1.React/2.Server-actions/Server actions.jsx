1ce43c7a978445dfb00bc2791b01494e3c9043b7
Klecianny Melo <klecianny.melo@quorum.us>
2026-04-27 21:16:50 -0300

    :sparkles: server actions

---
+++ 2.Frameworks/1.React/2.Server-actions/Server actions.jsx
@@ -0,0 +1,12 @@
+/* 🚀 SERVER ACTIONS */
+
+/* 👩🏾‍🚀 O QUE SÃO SERVER ACTIONS? */
+/*
+  Tradicionalmente, para salvar um dado, você precisava:
+
+  1. Criar um formulário no React.
+  2. Criar um endpoint POST no Node.js/Express.
+  3. Fazer um fetch no onSubmit.
+
+  Com Server Actions, você define uma função assíncrona com a diretiva "use server". O React entende que, ao chamar essa função, ele deve fazer uma requisição HTTP automática para o servidor, executar a lógica lá e devolver o resultado
+*/
